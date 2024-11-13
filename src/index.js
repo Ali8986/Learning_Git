3228f6bd89999cee1ec27e8ac26dc556b3daec9b
Ali Osama <aliusama8986@gmail.com>
2024-11-13 16:41:08 +0500

    I am learning Git Version COntrol System

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import image from "./images/4e6029bd-9cd4-4325-9eec-0b356f558a1e.jpg";
-ReactDOM.render(
-  <App />,
-
-  document.getElementById("root")
-);
+import image from "./images/4e6029bd-9cd4-4325-9eec-0b356f558a1e-removebg-preview.png";
+ReactDOM.render(<App />, document.getElementById("root"));
